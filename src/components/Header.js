609d6c96729a778a88d4d45c398128a2fa26f617
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-11-14 07:00:19 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ const Header = ({wrongLetters}) => {
     return (
         <>
             <h1>Finding a word</h1>
-            <p>Find the hidden word in 10 attemps - Enter a letter</p>
-            {wrongLetters.length ? <p>{wrongLetters.length} attemps</p>:''}
+            <p>Find the hidden word in 10 attempts - Enter a letter</p>
+            {wrongLetters.length ? <p>{wrongLetters.length} attempts</p>:''}
         </>
     )
 }
